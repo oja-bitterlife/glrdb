f0e53adaf7c5382c04fecf8a0683fa7f541c4567
ozaki <bitter.life.club@gmail.com>
2026-03-12 20:52:25 +0900

    add: Scan for bare repositories in specified paths

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	// bolt "go.etcd.io/bbolt"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/pelletier/go-toml/v2" // /v2 を忘れずに
 )
@@ -23,7 +25,7 @@ type Config struct {
 }
 
 type Source struct {
-	Paths string `toml:"path"`
+	Path string `toml:"path"`
 }
 
 func loadConfig() (*Config, error) {
@@ -40,12 +42,56 @@ func loadConfig() (*Config, error) {
 	return &config, nil
 }
 
+func isBareRepo(path string) bool {
+	_, errObjects := os.Stat(filepath.Join(path, "objects"))
+	_, errRefs := os.Stat(filepath.Join(path, "refs"))
+	_, errConfig := os.Stat(filepath.Join(path, "config"))
+	_, errHead := os.Stat(filepath.Join(path, "HEAD"))
+	return errObjects == nil && errRefs == nil && errConfig == nil && errHead == nil
+}
+func scanRepositories(config *Config) error {
+	for _, src := range config.Sources {
+		fmt.Printf("Scanning: %s\n", src.Path)
+
+		// ここで走査を開始
+		filepath.WalkDir(src.Path, func(path string, d os.DirEntry, err error) error {
+			if err != nil || !d.IsDir() {
+				return nil
+			}
+
+			// 高速化のため、いくつかのディレクトリをスキップ
+			// ----------------------------------------
+			name := d.Name()
+
+			// ドットで始まるディレクトリはスキップ
+			if strings.HasPrefix(d.Name(), ".") {
+				return filepath.SkipDir
+			}
+			// node_modulesとか、普通のディレクトリ名でドンと居座るのやめて(´・ω:;.:...
+			if name == "node_modules" || name == "vendor" {
+				return filepath.SkipDir
+			}
+
+			// Bareリポジトリを見つけたら
+			if isBareRepo(path) {
+				fmt.Printf("Found Bare Repo: %s\n", path)
+				// TODO: descriptionを抜き取ってbboltへ保存
+				return filepath.SkipDir // リポジトリの中までは掘らない
+			}
+			return nil
+		})
+	}
+	return nil
+}
+
 func main() {
 	config, err := loadConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	// debug print config
-	fmt.Printf("Loaded config: %+v\n", config)
+	err = scanRepositories(config)
+	if err != nil {
+		panic(err)
+	}
 }
